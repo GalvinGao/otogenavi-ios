dbb8c6699ece069c81b2d0dbfce95c331a6a0714
GalvinGao <me@galvingao.com>
2024-05-27 23:40:26 +0800

    Initial Commit

---
+++ OtogeNavi/OtogeNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtogeNaviApp.swift
+//  OtogeNavi
+//
+//  Created by Galvin Gao on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct OtogeNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
